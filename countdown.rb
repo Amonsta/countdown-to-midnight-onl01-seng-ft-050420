bcf521ca1baaa0f89fd95c3fa53aaa097a217fa7
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-10 05:09:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(sleep)
+  puts "sleep(.1)"
+end
+
+countdown_with_sleep(sleep)
+countdown(10)
+
+
+
